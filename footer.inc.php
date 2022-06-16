11527d1b6fc82a07aa9972e0f16efcde2340e7a3
Vlad <35457854+VladRudenok@users.noreply.github.com>
2022-06-16 22:51:27 +0300

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+            <h2>Ура, мы изучили основы PHP!</h2>
+        </div>
---
+++ menu.inc.php
@@ -0,0 +1,9 @@
+            <nav>
+                <ul>
+                    <li><a href="#"> My studies </a></li>
+                    <li><a href="#"> My portfolio </a></li>
+                    <li><a href="#"> My articles </a></li>
+                    <li><a href="#"> My links </a></li>
+                    <li><a href="#"> My contacts </a></li>
+                </ul>    
+            </nav>
